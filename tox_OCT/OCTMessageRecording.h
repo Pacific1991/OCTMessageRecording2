3984d8d04792f498fd6d781cb5771f556df76d30
孙栋栋 <xiao6121546@163.com>
2016-10-17 10:10:18 +0800

    0987654

--- tox_OCT/OCTMessageRecording.h
+++ tox_OCT/OCTMessageRecording.h
@@ -2,33 +2,12 @@
 //  OCTMessageRecording.h
 //  Pods
 //
-//  Created by Kihao on 2016/10/12.
+//  Created by Kihao on 2016/10/17.
 //
 //
 
-#import "OCTObject.h"
-#import "OCTToxConstants.h"
+#import <Foundation/Foundation.h>
 
-@class OCTToxConstants;
-
-@interface OCTMessageRecording : OCTObject
-@property (nullable) NSString *recording;
-@property (nonatomic, assign) float time;
-/**
- * Indicate if message is delivered. Actual only for outgoing messages.
- */
-@property BOOL isDelivered;
-@property  NSTimeInterval callDuration;
-
-/**
- * Type of the message.
- */
-@property OCTToxMessageType type;
-
-@property OCTToxMessageId messageId;
+@interface OCTMessageRecording : NSObject
 
 @end
-
-RLM_ARRAY_TYPE(OCTMessageRecording)
-
-
--- tox_OCT/OCTMessageRecording.m
+++ tox_OCT/OCTMessageRecording.m
@@ -2,26 +2,12 @@
 //  OCTMessageRecording.m
 //  Pods
 //
-//  Created by Kihao on 2016/10/12.
+//  Created by Kihao on 2016/10/17.
 //
 //
 
 #import "OCTMessageRecording.h"
 
-@interface OCTMessageRecording ()
-
-@end
-
 @implementation OCTMessageRecording
-
-- (NSString *)description
-{
-    NSString *description = [super description];
-    
-    const NSUInteger maxSymbols = 3;
-    NSString *text = self.recording.length > maxSymbols ? ([self.recording substringToIndex:maxSymbols]) : @"";
-    
-    return [description stringByAppendingFormat:@"OCTMessageText %@..., length %lu", text, (unsigned long)self.recording.length];
-}
 
 @end
